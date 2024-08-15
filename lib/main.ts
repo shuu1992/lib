634e81f8dc4e1f4d0eea69d99dd1978ce8c1017a
shu <wspkiki1992@cycu.org.tw>
2024-08-15 14:11:51 +0800

    AnimateButton

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,5 @@
 import '@i18n/index';
 
-export { Button } from './components/Button';
 export { Input } from './components/Input';
 export { Label } from './components/Label';
 export { SearchDateTimeRangePicker } from './components/search/DateTimeRangePicker';
