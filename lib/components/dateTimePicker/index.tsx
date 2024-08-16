9c44881cd95baa58443246f320090ec9d8bd6348
shu <wspkiki1992@cycu.org.tw>
2024-08-16 14:19:26 +0800

    fix testing theme in SearchDateTimeRangePicker v6

--- lib/components/dateTimePicker/index.tsx
+++ lib/components/dateTimePicker/index.tsx
@@ -1,15 +1,21 @@
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import CustomLayout from './layout';
 import ActionBar from './actionbar';
+import { ThemeProvider, useTheme } from '@mui/material/styles';
+
 export default function CustomizedDateTimePicker(props: any) {
+  const theme = useTheme();
+  console.error(theme);
   return (
-    <DateTimePicker
-      slots={{
-        layout: CustomLayout,
-        actionBar: ActionBar,
-      }}
-      dayOfWeekFormatter={(_day, weekday: any) => `${weekday.format('dd')}`}
-      {...props}
-    />
+    <ThemeProvider theme={theme}>
+      <DateTimePicker
+        slots={{
+          layout: CustomLayout,
+          actionBar: ActionBar,
+        }}
+        dayOfWeekFormatter={(_day, weekday: any) => `${weekday.format('dd')}`}
+        {...props}
+      />
+    </ThemeProvider>
   );
 }
